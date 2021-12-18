fe208ddef89fcf5ae8406b876f1c27584fd5104f
karl <elrion018@gmail.com>
2021-12-18 16:00:19 +0900

    [feature] then 메서드 선언

--- src/MyPromise.ts
+++ src/MyPromise.ts
@@ -4,7 +4,7 @@ enum PromiseStates {
   REJECTED = "REJECTED",
 }
 
-// 프로미스 클래스
+/** 프로미스 클래스 */
 export class MyPromise {
   state: string;
   value: any;
@@ -16,9 +16,32 @@ export class MyPromise {
     excecutor(this.resolve, this.reject);
   }
 
-  // 프로미스를 fulfilled 상태로 settled 시키는 메소드
-  resolve() {}
+  /** 프로미스 인스턴스를 이행(fulfilled) 상태로 귀결(settled) 시키는 메소드 */
+  resolve(value: any) {
+    // 프로미스 인스턴스가 이미 귀결 상태라면 무시해주기
+    if (
+      this.state === PromiseStates.FULFILLED ||
+      this.state === PromiseStates.REJECTED
+    )
+      return;
 
-  // 프로미스를 rejected 상태로 settled 시키는 메소드
-  reject() {}
+    this.state = PromiseStates.FULFILLED;
+    this.value = value;
+  }
+
+  /** 프로미스 인스턴스를 거절(rejected) 상태로 귀결시키는 메소드 */
+  reject(value: any) {
+    // 프로미스 인스턴스가 이미 귀결 상태라면 무시해주기
+    if (
+      this.state === PromiseStates.FULFILLED ||
+      this.state === PromiseStates.REJECTED
+    )
+      return;
+
+    this.state = PromiseStates.REJECTED;
+    this.value = value;
+  }
+
+  /** 프로미스 귀결 시 호출될 callback 들을 등록하는 메소드 */
+  then(onFulfilled, onRejected) {}
 }
