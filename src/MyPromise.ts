d0ec96802b3dfae2b5c3fa9172ea8122f51daf8d
karl <elrion018@gmail.com>
2021-12-18 20:17:27 +0900

    [feature] then으로 콜백 등록시 비동기적으로 실행하도록 수정

--- src/MyPromise.ts
+++ src/MyPromise.ts
@@ -9,7 +9,8 @@ enum PromiseStates {
 interface CustomPromise {
   state: string;
   value: any;
-  callbacks: Array<Function>;
+  fulfilledCallbacks: Array<Function>;
+  rejectedCallbacks: Array<Function>;
 
   resolve: resolve;
   reject: reject;
@@ -28,7 +29,7 @@ interface reject {
 
 /** then 메소드 인터페이스 */
 interface then {
-  (onFulfilled: Function, onRejected?: Function);
+  (onFulfilled?: Function, onRejected?: Function);
 }
 
 /** 프로미스 생성자가 파라미터로 갖는 excecutor 함수의 인터페이스 */
@@ -40,56 +41,94 @@ interface excecutor {
 export class MyPromise implements CustomPromise {
   state: string;
   value: any;
-  callbacks: Function[];
+  fulfilledCallbacks: Array<Function>;
+  rejectedCallbacks: Array<Function>;
 
   constructor(excecutor: excecutor) {
     this.state = PromiseStates.PENDING;
     this.value = undefined;
-    this.callbacks = [];
+    this.fulfilledCallbacks = [];
+    this.rejectedCallbacks = [];
 
-    // 비동기성을 보장하기 위해 setTimeout을 이용해 태스크 큐의 맨 뒤로 보내버리기
+    excecutor(this.resolve.bind(this), this.reject.bind(this));
+  }
+  /** 프로미스 인스턴스를 이행(fulfilled) 상태로 귀결(settled) 시키는 메소드 */
+  resolve(value: any) {
     setTimeout(
       function () {
-        excecutor(this.resolve.bind(this), this.reject.bind(this));
+        // 프로미스 인스턴스가 이미 귀결 상태라면 무시해주기
+        if (this.state !== PromiseStates.PENDING) return;
+
+        // 프로미스 인스턴스의 상태와 귀결값 설정
+        this.state = PromiseStates.FULFILLED;
+        this.value = value;
+
+        // then으로 등록된 콜백들을 실행시킨다.
+        this.fulfilledCallbacks.forEach((callback) => {
+          callback(this.value);
+        }, this);
       }.bind(this),
       0
     );
   }
 
-  /** 프로미스 인스턴스를 이행(fulfilled) 상태로 귀결(settled) 시키는 메소드 */
-  resolve(value: any) {
-    // 프로미스 인스턴스가 이미 귀결 상태라면 무시해주기
-    if (
-      this.state === PromiseStates.FULFILLED ||
-      this.state === PromiseStates.REJECTED
-    )
-      return;
-
-    // 프로미스 인스턴스의 상태와 귀결값 설정
-    this.state = PromiseStates.FULFILLED;
-    this.value = value;
-
-    this.callbacks.forEach((callback) => {
-      callback(this.value);
-    }, this);
-  }
-
   /** 프로미스 인스턴스를 거절(rejected) 상태로 귀결시키는 메소드 */
   reject(value: any) {
     // 프로미스 인스턴스가 이미 귀결 상태라면 무시해주기
-    if (
-      this.state === PromiseStates.FULFILLED ||
-      this.state === PromiseStates.REJECTED
-    )
-      return;
+    if (this.state !== PromiseStates.PENDING) return;
 
     // 프로미스 인스턴스의 상태와 귀결값 설정
     this.state = PromiseStates.REJECTED;
     this.value = value;
+
+    // then으로 등록된 콜백들을 실행시킨다.
+    this.rejectedCallbacks.forEach((callback) => {
+      callback(this.value);
+    }, this);
   }
 
   /** 프로미스 귀결 시 호출될 callback 들을 등록하는 메소드 */
+  /** 체이닝을 위해 프로미스 인스턴스를 반환해야한다. */
+  /** 등록된 콜백의 결과값은 반환된 프로미스가 또 다시 귀결시킨다. */
   then(onFulfilled?: Function, onRejected?: Function) {
-    this.callbacks = this.callbacks.concat([onFulfilled]);
+    return new MyPromise(
+      function (resolve, reject) {
+        this.fulfilledCallbacks.push(() => {
+          try {
+            // 이행 콜백의 결과값 구하기
+            const value = onFulfilled(this.value);
+
+            // value가 프로미스 객체라면
+            if (MyPromise.prototype.isPrototypeOf(value)) {
+              value.then(resolve, reject);
+
+              return;
+            }
+
+            resolve(value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+
+        this.rejectedCallbacks.push(() => {
+          try {
+            // 거절 콜백의 결과값 구하기
+            const value = onRejected(this.value);
+
+            // value가 프로미스 객체라면
+            if (MyPromise.prototype.isPrototypeOf(value)) {
+              value.then(resolve, reject);
+
+              return;
+            }
+
+            reject();
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }.bind(this)
+    );
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import { MyPromise } from "./MyPromise.js";
 
 const promise = new MyPromise((resolve, reject) => {
-  console.log("excecutor!");
-  resolve("Hello!");
-}).then((value) => {
-  console.log(value);
+  console.log("excecutor 호출!!");
+
+  setTimeout(function () {
+    console.log("귀결!");
+    resolve("값");
+  }, 1000);
 });
+promise
+  .then((value) => {
+    console.log(value, "첫 번째 분기");
+    return value;
+  })
+  .then((value) => {
+    console.log(value, "첫 번째 분기 체이닝1");
+    return value;
+  })
+  .then((value) => {
+    console.log(value, "첫 번째 분기 체이닝2");
+    return value;
+  });
+
+promise
+  .then((value) => {
+    console.log(value, "두 번째 분기");
+
+    return value;
+  })
+  .then((value) => {
+    console.log(value, "두 번째 분기 체이닝1");
+  });
