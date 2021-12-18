cbe1cbe73d1dd1987ea011a4a0a38f8f50f87419
karl <elrion018@gmail.com>
2021-12-18 15:40:06 +0900

    [feature] 클래스 스켈레톤 코드 구성

--- src/MyPromise.ts
+++ src/MyPromise.ts
@@ -1,6 +1,24 @@
+enum PromiseStates {
+  PENDING = "PENDING",
+  FULFILLED = "FULFILLED",
+  REJECTED = "REJECTED",
+}
 
+// 프로미스 클래스
 export class MyPromise {
-  constructor() {
-    
+  state: string;
+  value: any;
+
+  constructor(excecutor) {
+    this.state = PromiseStates.PENDING;
+    this.value = undefined;
+
+    excecutor(this.resolve, this.reject);
   }
+
+  // 프로미스를 fulfilled 상태로 settled 시키는 메소드
+  resolve() {}
+
+  // 프로미스를 rejected 상태로 settled 시키는 메소드
+  reject() {}
 }
